86fcf3bf54954b8bbadeb44ec308dfc5cd8e61ed
Yann Harel <yharel109@gmail.com>
2024-04-17 16:51:00 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This repo contains the code used to analyse learningg play sessions of SuperMarioBros. The dataset used in this study are part of the Courtois-Neuromod databank[1;5',
+    author='Yann Harel',
+    license='MIT',
+)
